2c83a0e717287861261d442110e2b0012d7a6e8d
Wyatt <wyatthroe@gmail.com>
2026-05-20 01:45:09 -0400

    committing all files from godot

---
+++ global/events.gd
@@ -0,0 +1,25 @@
+extends Node
+
+#card related events
+signal card_aim_started(card_ui: CardUI)
+signal card_aim_ended(card_ui:CardUI)
+signal card_played(card: Card)
+signal card_drag_started(card_ui: CardUI)
+signal card_drag_ended(card_ui: CardUI)
+signal card_tooltip_requested(card: Card)
+signal tooltip_hide_requested
+
+#player events
+signal player_hand_drawn
+signal player_hand_discarded
+signal player_turn_ended
+signal player_died
+signal player_hit
+
+
+#enemy events
+signal enemy_action_completed(enemy: Enemy)
+signal enemy_turn_ended
+
+#battle events
+signal battle_over_screen_requested(text: String, type: BattleOverPanel.Type)
